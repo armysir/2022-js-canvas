8e631317216d108ee7b449cd0c201055a858e4f4
armysir <102854930+armysir@users.noreply.github.com>
2022-09-26 13:51:23 +0900

    9-26

--- js/app.js
+++ js/app.js
@@ -7,9 +7,13 @@ const colorOption = Array.from(document.getElementsByClassName("color"));
 const destroyBtn = document.getElementById("destroy-btn");
 const file =document.getElementById("file");
 const eraserBtn = document.getElementById("eraser-btn");
+const textInput =document.getElementById("text");
+const saveBtn = document.getElementById("save")
 canvas.width = 500;
 canvas.height = 500;
+ctx.lineCap = "round"
 ctx.lineWidth=lineWidth.value;
+
 let isPainting = false;
 let isFilling = false;
 
@@ -92,11 +96,46 @@ function onClickEraser (){
         modeBtn.innerText = "채우기"
     }
 }
+function onFileChange(event){
+    
+
+    console.dir(event.target)
+    const files = event.target.files[0];
+    const url = URL.createObjectURL(files);
+    console.log(url);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image,0,0,500,500)
+        
+    }
+}
+function onDb(event){
+    ctx.save();
+    console.log(textInput.value)
+    text = textInput.value;
+    ctx.font="20px sefif"
+    ctx.lineWidth=1;
+    ctx.fillText(text,event.offsetX,event.offsetY);
+    console.log(event.offsetX,event.offsetY)
+    ctx.restore();
+
+}
+function onSaveClick (){
+   const url = canvas.toDataURL();
+   const a = document.createElement("a")
+   a.href = url ;
+   a.download = "내 그림";
+   a.click();
+   
+
+}
 canvas.addEventListener("mousemove",onClick)
 canvas.addEventListener("mousedown",onMouse)
 canvas.addEventListener("mouseup",onMouseUp)
 canvas.addEventListener("mouseleave",onMouseUp)
 canvas.addEventListener("click",onCanvasFill);
+canvas.addEventListener("dblclick",onDb);
 
 
 lineWidth.addEventListener("change",onLineWidthChange)
@@ -108,3 +147,4 @@ modeBtn.addEventListener("click",onModeClick)
 destroyBtn.addEventListener("click",onCanvasDestroy);
 eraserBtn.addEventListener("click",onClickEraser);
 file.addEventListener("change",onFileChange)
+saveBtn.addEventListener("click",onSaveClick);
